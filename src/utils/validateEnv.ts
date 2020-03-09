90810c9cebdb5e214f56db7661ee59d6f2d57e52
Adal Vega <avega@aidacanarias.com>
2020-03-09 22:19:53 +0000

    docker-compse

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,7 +3,6 @@ import { cleanEnv, port, str } from 'envalid';
 function validateEnv() {
   cleanEnv(process.env, {
     NODE_ENV: str(),
-    JWT_SECRET: str(),
     PORT: port(),
     MONGODB_URI: str()
   });
